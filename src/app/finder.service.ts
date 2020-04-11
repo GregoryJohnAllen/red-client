b560985ab346646049241b5b4a3e1e9fea9b3c93
Greg Allen <gregoryjohnallen94@gmail.com>
2020-04-11 13:29:38 -0400

    heroku commit

--- src/app/finder.service.ts
+++ src/app/finder.service.ts
@@ -7,17 +7,26 @@ import { HttpClient } from '@angular/common/http';
 export class FinderService {
 
   constructor(private http: HttpClient) { }
-
+  //get all user profiles
+  getAllProfiles() {
+    return this.http.get(`http://localhost:3000/user/getall`)
+  }
+  getMyUser(userid) {
+    return this.http.get(`http://localhost:3000/user/${userid}`)
+  }
   //get all for Finder Profiles for news feed
   getFinder() {
     return this.http.get("http://localhost:3000/seeker/")
   }
   //get specific Finder profile that you want to view
-  getFinderProfile() {
-    return this.http.get("http://localhost:3000/seeker/:id")
+  getFinderProfile(userid) {
+    return this.http.get(`http://localhost:3000/seeker/${userid}`)
+  }
+  getMySeeker(userid) {
+    return this.http.get(`http://localhost:3000/finder/${userid}`)
   }
   //update by id for the seeker profile that you are logged into
-  updateSeeker() {
-    return this.http.get("http://localhost:3000/seeker/update/:id")         
+  updateSeeker(userid) {
+    return this.http.get(`http://localhost:3000/seeker/update/${userid}`)         
   }
 }
--- src/app/seeker.service.ts
+++ src/app/seeker.service.ts
@@ -7,17 +7,28 @@ import { HttpClient } from '@angular/common/http';
 export class SeekerService {
 
   constructor(private http: HttpClient) { }
+  //get all user profiles
+  getAllProfiles() {
+    return this.http.get(`http://localhost:3000/user/getall`)
+  }
+  //get the user profile of yourself
+  getMyUser(userid) {
+    return this.http.get(`http://localhost:3000/user/${userid}`)
+  }
 
   //get all for Seeker Profiles for news feed
   getSeeker() {
     return this.http.get("http://localhost:3000/finder/")
   }
   //get specific Seeker profile that you want to view
-  getSeekerProfile() {
-    return this.http.get("http://localhost:3000/finder/:id")
+  getSeekerProfile(userid) {
+    return this.http.get(`http://localhost:3000/finder/${userid}`)
+  }
+  getMyFinder(userid) {
+    return this.http.get(`http://localhost:3000/seeker/${userid}`)
   }
   //update by id for the finder profile that you are logged into
-  updateFinder() {
-    return this.http.get("http://localhost:3000/finder/update/:id")         
+  updateFinder(userid) {
+    return this.http.get(`http://localhost:3000/finder/update/${userid}`)         
   }
 }
